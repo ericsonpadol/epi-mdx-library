6757f20d6074c27d950c024cda1b4e20b0ff65de
Ericson Padol <ericson.padol@gmail.com>
2021-03-21 16:08:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import commons from './src/commons';
 import copywrites from './src/copywrites';
 import errors from './src/commons/errors';
 
-export { commons, copywrites, errors };
+export default { commons, copywrites, errors };
